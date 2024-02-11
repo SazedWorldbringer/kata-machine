317fa43ba98015ca1513b6e52dc5d678a365666c
Atharva Pardeshi <atharva.exe@gmail.com>
2024-02-12 01:00:14 +0530

    feat: implement quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,33 @@
-export default function quick_sort(arr: number[]): void {
+const quickSort = (arr: number[], lo: number, hi: number): void => {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+
+    quickSort(arr, lo, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, hi);
+}
+
+const partition = (arr: number[], lo: number, hi: number): number => {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            let tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    quickSort(arr, 0, arr.length - 1)
 }
